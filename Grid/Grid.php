bb8f59f0566191be4f56117d3e91ac02d153c324
Daum <matt@setfive.com>
2022-09-14 16:53:54 -0400

    Twig render vs render respond upgrade

--- Grid/Grid.php
+++ Grid/Grid.php
@@ -2128,7 +2128,7 @@ class Grid implements GridInterface
             if ($view === null) {
                 return $parameters;
             } else {
-                return $this->container->get('templating')->renderResponse($view, $parameters, $response);
+                return new Response($this->container->get('twig')->render($view, $parameters, $response));
             }
         }
     }
